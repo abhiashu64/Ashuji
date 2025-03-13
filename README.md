6c67e0a2bd862a6f49bd6c0f7a8c45d2bf1defff
abhiashu64 <cookiesabhi0064@gmail.com>
2025-03-13 06:17:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome Dosto....
+# non drm bot made by ༄ᶦᶰᵈ᭄࿐𑁍IFS𑁍(आशु)❥◉🇮🇳™
+# Edit Vars.py
+# Change API_ID, API_HASH
+# DON'T PASTE BOT_TOKEN
+# BOT_TOKEN USE ONLY HOSTING SERVER
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/lakshaybajarang/Lakshay_Paid.git)
---
+++ vars.py
@@ -0,0 +1,10 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "29458676")
+API_HASH  = os.environ.get("API_HASH", "5f1bd56c8a4796ab1ff6999ff2fdadc4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7837030154:AAGl9pv8Ak2wEFbnjbakETr0Jk77tV0zXEw") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
